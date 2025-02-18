4f2377431aea386cf1efc0f3e0b2be4db5d678d0
Anis Sarkar <anissarkar.klt@gmail.com>
2025-02-18 11:42:35 +0530

    Added .env file and updated settings for local development

--- social_manager/settings.py
+++ social_manager/settings.py
@@ -26,10 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=y5$7%8g)u2_s^u#+vy8ips58fu980@zzhoapkbq%!0f6%7q0x'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-local-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
